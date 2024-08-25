4f6389069bebb350c5ac23170b0dd00e5d74eb2a
HOchacha <outcider112@dankook.ac.kr>
2024-08-25 21:38:05 +0900

    fix: give correct argument to ossl function

--- bssl-compat/source/SSL_CTX_set_ciphersuites.cc
+++ bssl-compat/source/SSL_CTX_set_ciphersuites.cc
@@ -3,5 +3,5 @@
 #include "iana_2_ossl_names.h"
 
 extern "C" int SSL_CTX_set_ciphersuites(SSL_CTX *ctx, const char *str) {
-  return ossl.ossl_SSL_CTX_set_ciphersuites(ctx, osslstr.c_str());
+  return ossl.ossl_SSL_CTX_set_ciphersuites(ctx, str);
 }
